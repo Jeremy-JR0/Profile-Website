50e8c5ed84ea92205b60f81582890c9b4f51813f
Jeremy-JR0 <shadow102024@bigpond.com>
2025-02-20 11:27:18 +0800

    Added more to projects page (buttons, content and changed CSS)

--- Javascript/projectsJS.js
+++ Javascript/projectsJS.js
@@ -6,3 +6,47 @@ function toggleSidebar() {
         sidebar.style.left = "0px";
     }
 }
+
+function buttonFunctionFolio() {
+    var x = document.getElementById("portfolioInfo");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function buttonFunctionIOT() {
+    var x = document.getElementById("IOT");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function buttonFunctionRetro() {
+    var x = document.getElementById("Retro");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function buttonFunctionCapstone() {
+    var x = document.getElementById("Capstone");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function buttonFunctionKR() {
+    var x = document.getElementById("KR");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
