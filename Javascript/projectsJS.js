74c2d26294bab889d2fb01e61e0968f6aa1a99b0
Jeremy-JR0 <shadow102024@bigpond.com>
2025-02-23 11:46:52 +0800

    Made changes to project page - added project grid to display projects, change nav bar to fit new page (had to adjust all pages), added overlay to display content

--- Javascript/projectsJS.js
+++ Javascript/projectsJS.js
@@ -7,46 +7,13 @@ function toggleSidebar() {
     }
 }
 
-function buttonFunctionFolio() {
-    var x = document.getElementById("portfolioInfo");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-function buttonFunctionIOT() {
-    var x = document.getElementById("IOT");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function onOverlay(overlayId) {
+    document.getElementById(overlayId).style.display = "flex";
+    document.body.style.overflow = "hidden"; // Prevent background scroll
 }
 
-function buttonFunctionRetro() {
-    var x = document.getElementById("Retro");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function offOverlay(overlayId) {
+    document.getElementById(overlayId).style.display = "none";
+    document.body.style.overflow = "auto"; // Re-enable background scroll
 }
 
-function buttonFunctionCapstone() {
-    var x = document.getElementById("Capstone");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
-function buttonFunctionKR() {
-    var x = document.getElementById("KR");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
