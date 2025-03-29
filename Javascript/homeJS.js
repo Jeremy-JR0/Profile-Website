94f196fab3a65fb0a3206ef16aea5f17941d7fda
Jeremy-JR0 <shadow102024@bigpond.com>
2025-03-29 17:25:53 +0800

    Fixed issue with sidebar not working

--- Javascript/homeJS.js
+++ Javascript/homeJS.js
@@ -1,8 +1,9 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     if (sidebar.style.left === "0px") {
-        sidebar.style.left = "-250px";
+      sidebar.style.left = "-250px";
     } else {
-        sidebar.style.left = "0px";
+      sidebar.style.left = "0px";
     }
-}
+  }
+  
